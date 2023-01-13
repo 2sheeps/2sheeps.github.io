84ad3a47e4fe1b4b1f2152e29b4bb469564c5e2e
Xianchao Yang <xianchao.yang@qq.com>
2023-01-13 17:39:21 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="@/assets/logo.png">
+    <h3>200000000</h3>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+#app {}
+</style>
