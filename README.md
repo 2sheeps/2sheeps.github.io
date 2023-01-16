7702f4c17aec2df29dacdee3219b4ae286ac6280
Xianchao Yang <xianchao.yang@qq.com>
2023-01-16 14:41:53 +0800

    uniapp

---
+++ README.md
@@ -0,0 +1,8 @@
+# 2sheeps.github.io
+2sheeps.github.io
+
+1. npm run build
+2. git branch gh-pages
+3. git checkout gh-pages
+4. git add -f unpackage/dist/build/h5
+5. git subtree push --prefix unpackage/dist/build/h5 origin gh-pages
---
+++ pages/me/me.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<image src="../../static/logo.png" mode=""></image>
+		
+		<navigator url="../index/index">
+			index
+		</navigator>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
